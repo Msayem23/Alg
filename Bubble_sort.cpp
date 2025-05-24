0d73953c4b5f855738b8013f7de486adbb65cc36
Msayem23 <144926195+Msayem23@users.noreply.github.com>
2025-05-24 23:53:48 +0600

    Update Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -11,7 +11,7 @@ int main(){
     }
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1; j++){
-        //asscending
+        
             if(arr[j]>arr[j+1]){ //for descending arr[j]<arr[j+1]
             int temp=arr[j];
             arr[j]=arr[j+1];
@@ -26,5 +26,3 @@ int main(){
     }
     return 0;
 }
-//time complexit best O(n), worst O(n^2), average O(n^2)
-//space complexity O(1)
